f02107359bb2aa4b38924a70eae0e7a011d02666
thehoneymad <thehoneymad@hotmail.com>
2014-10-10 21:35:24 +0600

    GAMESERV - 02, finally started writing

--- Lib/RealmFactory.js
+++ Lib/RealmFactory.js
@@ -1,20 +1,44 @@
+//This is only for test function, take it off after unit test
+var pRequire = require("../projRequire.js");
+//
 
+
+var Realms = [];
+var MAXROOMS = 50; //Dude you need a constant library;
+
+///CreateRealm - Creates a Game Room for specified type
+///
+///
+function CanCreateRealm()
+{
+    return (Realms.length >= MAXROOMS) ;
+}
 
 function CreateRealm(type)
 {
-    var requiredModule;
-    try {
-        requiredModule = projRequire("Lib.Realms." + type + "Realm"); //This definitely will be handled better;
-        console.log("The address to the main module is "+ require.main.filename);
-    }
+    if (!CanCreateRealm()) {
+        
+        var requiredModule;
+        try {
+            requiredModule = projRequire("Lib.Realms." + type + "Realm"); //This definitely will be handled better;
+            var MonopolyObj = new requiredModule.MonopolyObject();
+            Realms.push(MonopolyObj);
+            //I need to push the room here
+        }
     catch (err) {
-        console.log(err);
+            console.log(err);
+        }
+        
+        
+        
+        return requiredModule? requiredModule: false;
     }
-    
-    return requiredModule? requiredModule: false;
+    else
+        return false; // Have to decide what should I return when rooms are maxed out // may be a seperate method canCreateRealm?
+
 }
 
-CreateRealm("Monopoly");
+
 
 module.exports.testRealmCreation = function (test){
     var ret = CreateRealm("Monopoly");
@@ -23,3 +47,4 @@ module.exports.testRealmCreation = function (test){
     test.done();
 
 }
+
--- Lib/Realms/MonopolyRealm.js
+++ Lib/Realms/MonopolyRealm.js
@@ -1,8 +1,14 @@
 ﻿//Package MonopolyRealm.js
 var express = require('express');
 
+
+function MonopolyRealm()
+{
+    this.description = "This is going to be the monopoly game object";
+}
 //We need to strategise what we are going to do here of course
 
 module.exports = {
     description: "This is definitely going to the module for monopoly"
 };
+module.exports.MonopolyObject = MonopolyRealm;
